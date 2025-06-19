0f49fb9c307e6143d399ad0172b78d4afdc3823d
Techlabs-jeonhaedong <119987200+Techlabs-jeonhaedong@users.noreply.github.com>
2025-06-19 09:48:44 +0900

    pod update

--- Pods/Target Support Files/Pods-iOSSample/Pods-iOSSample-frameworks.sh
+++ Pods/Target Support Files/Pods-iOSSample/Pods-iOSSample-frameworks.sh
@@ -178,10 +178,12 @@ code_sign_if_enabled() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/BidmadSDK/BidmadSDK.framework"
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/LuckyBiteiOS/LuckyVerse.framework"
+  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/OpenBiddingHelper/OpenBiddingHelper.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/BidmadSDK/BidmadSDK.framework"
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/LuckyBiteiOS/LuckyVerse.framework"
+  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/OpenBiddingHelper/OpenBiddingHelper.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
