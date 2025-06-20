fae3d76fb51eb89d8e29c398eb73182a0abecd5c
Techlabs-jeonhaedong <119987200+Techlabs-jeonhaedong@users.noreply.github.com>
2025-06-20 10:49:11 +0900

    Stop tracking Pods directory

--- Pods/BidmadSDK/BidmadSDK.xcframework/ios-x86_64-simulator/BidmadSDK.framework/Headers/BidmadAdType.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-typedef NS_CLOSED_ENUM(NSInteger, BidmadAdType) {
-    BidmadAdTypeBanner,
-    BidmadAdTypeInterstitial,
-    BidmadAdTypeReward,
-    BidmadAdTypeNative,
-    BidmadAdTypeAppOpen,
-};
--- Pods/OpenBiddingHelper/README.md
+++
@@ -1,4 +0,0 @@
-# BidmadSDK CocoaPods Change Logs
-## Version 2.6.3
-- BidmadSDK and OpenBiddingHelper Logics are merged
-- AdColony Adapter framework is now removed from dependency list
